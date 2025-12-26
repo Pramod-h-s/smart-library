93f183ebc56d4c299af6febcdfa429ee2aee9b66
Pramod-h-s <pramod.hadadi.s@gmail.com>
2025-12-27 03:25:15 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,20 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// js/firebase.js
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBKZfatQ30uJtBp-LyQQEJcgesmpVmt_pM",
-    authDomain: "smart-library-c2710.firebaseapp.com",
-    projectId: "smart-library-c2710",
-    storageBucket: "smart-library-c2710.firebasestorage.app",
-    messagingSenderId: "359933328798",
-    appId: "1:359933328798:web:a4fb9d35c3208fa7776a8f"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKZfatQ30uJtBp-LyQQEJcgesmpVmt_pM",
+  authDomain: "smart-library-c2710.firebaseapp.com",
+  projectId: "smart-library-c2710",
+  storageBucket: "smart-library-c2710.firebasestorage.app",
+  messagingSenderId: "359933328798",
+  appId: "1:359933328798:web:a4fb9d35c3208fa7776a8f"
+};
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
